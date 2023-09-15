f946ce3f8dae49f67bf971e424f82ad423775d42
Aleksandr-Gusev <gusev.rpa@gmail.com>
2023-09-15 21:51:11 +0300

    форматирование данных

--- Parse_word.py
+++ Parse_word.py
@@ -1,6 +1,29 @@
 import docx
 import os
 
+# ---------------------------------------------- функция форматирования даты ------------------------------
+
+def formating_date (stroka):
+    day = stroka[1:3]
+    print('День', day)
+    buf_month = stroka[5:13]
+    print('Месяц', buf_month)
+    year = stroka[14:18]
+    print('Год', year)
+    list_month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
+
+    count = 0
+    month = ''
+    for item in list_month:
+
+        if buf_month == item:
+            month = count+1
+            break
+        count += 1
+
+    print('месяц', month)
+
+
 # -----------------------------------------------поиск и запись всех путей файлов------------------------
 paths = []
 folder = os.path.dirname('C:\\Users\\Admin\\PycharmProjects\\AVA\\Acts')
@@ -49,9 +72,14 @@ for path in paths:
     number_act = mas_tables[0].cell(0, 0).text[-1]  # Номер акта
     number_zayavka = mas_tables[4].cell(0, 0).text[-1] # Номер заявки
     date_act = mas_tables[1].cell(0, 1).text  # Дата акта
+    formating_date(date_act)
     project_act = mas_tables[2].cell(1, 1).text  # Наименование проекта
     key_project_act = mas_tables[2].cell(1, 2).text  # Ключ проекта
+
     period = mas_tables[2].cell(1, 4).text  # период
+    date_start = period[2:12].replace('.', '/')  # дата начала
+    date_end = period[15:26].replace('.', '/')   # дата завершения
+
     time_act = mas_tables[2].cell(1, 5).text  # трудозатраты
     rate_act = mas_tables[2].cell(1, 6).text  # ставка в акте
     rate_zayavka = mas_tables[5].cell(1, 1).text  # ставка в заявке
@@ -68,6 +96,8 @@ for path in paths:
     print(project_act)  # Наименование проекта
     print(key_project_act)  # Ключ проекта
     print(period)  # период
+    print('Начало периода', date_start)
+    print('Конец периода', date_end)
     print(time_act)  # трудозатраты
     print(rate_act)  # ставка в Акте
     print(rate_zayavka)  # ставка в заявке
@@ -79,15 +109,16 @@ for path in paths:
 
 # -----------------------------------------------работа с текстом------------------------
 text = []
-for paragraph in doc.paragraphs:                   #получение списка параграфов
+for paragraph in doc.paragraphs:                   # получение списка параграфов
     text.append(paragraph.text)
 
 """print('\n'.join(text))"""
 
 
 
-# -----------------------------------------------выделение имени из акта------------------------
-full_name_act1 = text[5][text[5].index('ИП')+3:text[5].index(', именуемый')] #выделение имени
+# ----------------------------------------------- выделение имени из акта ------------------------
+
+full_name_act1 = text[5][text[5].index('ИП')+3:text[5].index(', именуемый')]   # выделение имени
 print(full_name_act1)
 
 name_act = full_name_act1[0:full_name_act1.rfind(' ')]
@@ -97,10 +128,10 @@ print(name_act)
 # -----------------------------------------------выделение суммы из текста акта------------------------
 
 
-rub = text[9][text[9].index('сумму ')+6:text[9].index(' (')] #выделение суммы руб
-copeyka = text[9][text[9].index(' копе')-2:text[9].index(' копе')] #выделение суммы копейки
+rub = text[9][text[9].index('сумму ')+6:text[9].index(' (')]   # выделение суммы руб
+copeyka = text[9][text[9].index(' копе')-2:text[9].index(' копе')]   # выделение суммы копейки
 print(rub)
 print(copeyka)
 
-total_cost_act_in_text = rub + ',' + copeyka # полная сумма
+total_cost_act_in_text = rub + ',' + copeyka   # полная сумма
 print(total_cost_act_in_text)
